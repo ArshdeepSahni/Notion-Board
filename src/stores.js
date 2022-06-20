6e0b7c503eb0ded2ea161fe43f5ef54ecd4a0b56
arsh22sahni <arsh22sahni@gmail.com>
2022-06-20 23:30:01 +0530

    deploy fixes

--- src/stores.js
+++ src/stores.js
@@ -11,27 +11,3 @@ import { writable } from "svelte/store";
 =======================================================================*/
 export const themeColor = writable();
 themeColor.set("stone");
-
-// export const calories = writable();
-// calories.set("0");
-// export const foodType = writable();
-// foodType.set("Eggetarian");
-
-/*=============================================================================================================
-  ad88888ba   88        88  88888888ba      db         88888888ba         db         ad88888ba   88888888888
- d8"     "8b  88        88  88      "8b    d88b        88      "8b       d88b       d8"     "8b  88
- Y8,          88        88  88      ,8P   d8'`8b       88      ,8P      d8'`8b      Y8,          88
- `Y8aaaaa,    88        88  88aaaaaa8P'  d8'  `8b      88aaaaaa8P'     d8'  `8b     `Y8aaaaa,    88aaaaa
-   `"""""8b,  88        88  88""""""'   d8YaaaaY8b     88""""""8b,    d8YaaaaY8b      `"""""8b,  88"""""
-         `8b  88        88  88         d8""""""""8b    88      `8b   d8""""""""8b           `8b  88
- Y8a     a8P  Y8a.    .a8P  88        d8'        `8b   88      a8P  d8'        `8b  Y8a     a8P  88
-  "Y88888P"    `"Y8888Y"'   88       d8'          `8b  88888888P"  d8'          `8b  "Y88888P"   88888888888
-=============================================================================================================*/
-export const supabaseUrl = writable("https://lkywmctcaxwouuanekxe.supabase.co");
-supabaseUrl.set("https://lkywmctcaxwouuanekxe.supabase.co");
-export const supabaseKey = writable(
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjQxMzY2NzI0LCJleHAiOjE5NTY5NDI3MjR9.kgiO_t-6vaS8XAK_NdjF9fLTnjzk5-gStTZ0eNtuNaU"
-);
-supabaseKey.set(
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjQxMzY2NzI0LCJleHAiOjE5NTY5NDI3MjR9.kgiO_t-6vaS8XAK_NdjF9fLTnjzk5-gStTZ0eNtuNaU"
-);
--- src/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_KEY
-);
-
-export default supabase;
